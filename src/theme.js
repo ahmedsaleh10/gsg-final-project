ff87fd603eaf439129d21af538167e92e7571780
Ahmed <ahmed.ovicsaleh@outlook.com>
2023-05-11 17:15:15 +0300

    installing mui and mui icons. and create ThemeProvider to warping it for all application

---
+++ src/theme.js
@@ -0,0 +1,57 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette:{
+        primary:{
+            dark: "#232C46", //Yankees Blue
+            light:"#725C4D", //Boy Red
+            main:"#694B38", //Coffee
+        },
+        secondary:{
+            main:"#BAD9C2", // light green
+            dark:"#ED843D", // Tiger's eye
+            light:"#60636D", // Dark green text color
+        },
+        
+    },
+
+    typography:{
+        fontFamily:'Poppins',
+        h1:{ 
+            fontSize:"6rem",
+            fontWeight:"700",
+        },
+        h2:{ 
+            fontSize:"5.5rem",
+            fontWeight:"400"
+        },
+        sectionTitle:{
+            fontSize:"4rem",
+            fontWeight:"700",
+        },
+        subtitle1:{
+            fontSize:"2.5rem",
+            fontWeight:"700"
+        },
+        body:{
+            fontSize:"1.25rem",
+            fontWeight:"400"
+        },
+        productText:{
+            fontSize:"2rem",
+            fontWeight:"700",
+        },
+        price:{
+            fontSize:"1.25rem",
+        },
+        bigPrice:{
+            fontSize:"2rem",
+        },
+        button:{
+            fontSize:"1.25rem",
+            fontWeight:"700"
+        },
+    }
+})
+
+export default theme
