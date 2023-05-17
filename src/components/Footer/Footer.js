5cd07eac25a34326dc76027611dcb954d2b16c3a
Ahmed <ahmed.ovicsaleh@outlook.com>
2023-05-18 01:23:52 +0300

    adding footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FooterBody from './FooterBody'
+
+const Footer = () => {
+  return (
+    <footer>
+        <FooterBody/>
+    </footer>
+  )
+}
+
+export default Footer
