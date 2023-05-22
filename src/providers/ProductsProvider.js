4535ec7b145b9738d997288431d6f46cd7449774
Ahmed Saleh <ahmed.ovicsaleh@outlook.com>
2023-05-22 18:27:02 +0300

    set the empty value better than null

--- src/providers/ProductsProvider.js
+++ src/providers/ProductsProvider.js
@@ -3,7 +3,7 @@ import ProductContext from "../contexts/ProductsContext";
 import axios from "axios";
 
 const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(null);
+  const [products, setProducts] = useState("");
   const getProducts = async () => {
     const response = await axios.get(
       "https://run.mocky.io/v3/1a8abb1a-5296-4fa0-b34e-dfbca6937bd3"
