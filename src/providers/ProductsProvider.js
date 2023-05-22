32e9ad6834836a7c35e86ca150d8730c8be02762
Ahmed Saleh <ahmed.ovicsaleh@outlook.com>
2023-05-22 17:23:24 +0300

    set the initial state with null value

--- src/providers/ProductsProvider.js
+++ src/providers/ProductsProvider.js
@@ -3,7 +3,7 @@ import ProductContext from "../contexts/ProductsContext";
 import axios from "axios";
 
 const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState(null);
   const getProducts = async () => {
     const response = await axios.get(
       "https://run.mocky.io/v3/89cea58a-97a5-4ab9-aefa-6ecd6021b483"
