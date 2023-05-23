558a9467aa38bce8f8335f2d039164b1e7661598
Ahmed Saleh <ahmed.ovicsaleh@outlook.com>
2023-05-23 12:33:47 +0300

    set loading for a section which need the a product from api not for all page

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,24 +1,27 @@
 import React, { useContext } from "react";
 import ProductCard from "../components/ProductCard/ProductCard";
 import ProductContext from "../contexts/ProductsContext";
-import TopNav from '../components/TopNav/TopNav'
-import Footer from "../components/Footer/Footer"
+import TopNav from "../components/TopNav/TopNav";
+import Footer from "../components/Footer/Footer";
 import Loading from "../components/LoadingProducts/Loading";
-
 
 const HomePage = () => {
   const products = useContext(ProductContext);
   if (products) {
     return (
       <>
-      <TopNav/>
+        <TopNav />
         <ProductCard product={products[0]} />
-        <Footer/>
+        <Footer />
       </>
     );
   } else {
     return (
-      <Loading/>
+      <>
+        <TopNav />
+        <Loading />
+        <Footer />
+      </>
     );
   }
 };
--- src/Pages/ProductDeatilsPage.js
+++ src/Pages/ProductDeatilsPage.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { useParams } from 'react-router'
 import ProductDetails from '../components/ProductDetails/ProductDetails'
 
-const ProductDeatilsPage = () => {
+const ProductDetailsPage = () => {
     
   const {id} = useParams()
   return (<ProductDetails key={id} productId={id}/> )
 }
 
-export default ProductDeatilsPage
+export default ProductDetailsPage
