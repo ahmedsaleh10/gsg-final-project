1914397bef46422bf883a89f63543fe367b1bd3e
Ahmed Saleh <ahmed.ovicsaleh@outlook.com>
2023-05-23 12:31:22 +0300

    products details page route

--- src/Routers.js
+++ src/Routers.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from './Pages/HomePage'
-import ProductDeatils from './Pages/ProductDeatilsPage'
+import ProductDeatilsPage from './Pages/ProductDeatilsPage'
 
 const Routing = () => {
   return (
     <BrowserRouter>
         <Routes>
             <Route index element={<HomePage/>}/>
-            <Route path='/products/:id' element={<ProductDeatils/>}/>
+            <Route path='/products/:id' element={<ProductDeatilsPage/>}/>
         </Routes>
     </BrowserRouter>
   )
