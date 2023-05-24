04ff0b981ab3b9211dbec0370f72af004efd8b58
Ahmed Saleh <ahmed.ovicsaleh@outlook.com>
2023-05-24 19:07:28 +0300

    implement the best sales section using swiper library

---
+++ src/components/BestSales/BestSales.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/grid";
+import "swiper/css/pagination";
+import { Grid } from "swiper";
+import ProductCard from '../ProductCard/ProductCard';
+import SliderButtons from './SliderButtons';
+import useStyles from './Styles';
+import {Typography } from "@mui/material";
+
+
+const BestSales = ({products}) => {
+    const classes = useStyles()
+    const salesProducts = products.filter((product)=> product.sale === true)
+  return (
+    <div className={classes.SliderContainer}>
+        <Typography variant='subtitle1'>Best Sales</Typography>
+        <Swiper
+            loop={true}
+            grid={{
+            rows: 1,
+            }}
+            modules={[Grid]}
+            className={classes.CustomSwiper}
+            breakpoints={{
+                300:{
+                    slidesPerView:1,
+                    grid:{
+                        rows:3
+                    },
+                    spaceBetween:20
+                },
+                600:{
+                    slidesPerView:2,
+                    grid:{
+                        rows:2
+                    },
+                    spaceBetween:30
+                },
+                1200:{
+                    slidesPerView:3,
+                    grid:{
+                        rows:1
+                    },
+                    spaceBetween:30
+                }
+            }}
+        >
+            {salesProducts.map((product)=>{
+                return(
+                    <SwiperSlide key={product.id}>{<ProductCard product={product} />}</SwiperSlide>
+                )
+            })}
+            <SliderButtons/>
+    </Swiper>
+    </div>
+   
+  )
+}
+
+export default BestSales
