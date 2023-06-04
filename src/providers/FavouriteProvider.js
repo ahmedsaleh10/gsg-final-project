da4a3a44d4c2c7b79f86820fd93e592fb416f6c7
Ahmed Saleh <ahmed.ovicsaleh@outlook.com>
2023-06-05 00:55:49 +0300

    adding shared logic for Favourite Context

--- src/providers/FavouriteProvider.js
+++ src/providers/FavouriteProvider.js
@@ -1,18 +1,35 @@
 import React from "react";
 import FavouriteContext from "../contexts/FavouriteContext";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 const FavouriteProvider = ({ children }) => {
   const [badgeNumber, setBadgeNumber] = useState(0);
   const [FavouriteProducts,setFavouriteProducts] = useState([])
+  
   const handleFavoriteButton = (product) => {
-    setBadgeNumber((prev) => prev + 1);
-    const newFavouriteProducts = [...FavouriteProducts,product]
-    setFavouriteProducts(newFavouriteProducts)
+    const includedProduct = FavouriteProducts.includes(product)
+    if(!includedProduct){
+      setBadgeNumber((prev) => prev + 1)
+      setFavouriteProducts([...FavouriteProducts,product])
+    }
   };
 
+  const handleDeleteFavouriteProduct = (productId) => {
+    setBadgeNumber(badgeNumber-1)
+    const newFavouriteProducts = FavouriteProducts.filter( (product) => product.id !== productId)
+    setFavouriteProducts(newFavouriteProducts)
+  }
+
+  useEffect(() => {
+    localStorage.setItem('products', JSON.stringify(FavouriteProducts));
+  }, [FavouriteProducts]);
+
+  useEffect(() => {
+    localStorage.setItem('badge', JSON.stringify(badgeNumber));
+  }, [badgeNumber]);
+
   return (
-    <FavouriteContext.Provider value={{ badgeNumber,FavouriteProducts, handleFavoriteButton }}>
+    <FavouriteContext.Provider value={{ badgeNumber,FavouriteProducts, handleFavoriteButton , handleDeleteFavouriteProduct }}>
       {children}
     </FavouriteContext.Provider>
   );
