c2b37026821ee504ae67c203e4dd552d1a7e21b2
Ahmed Saleh <ahmed.ovicsaleh@outlook.com>
2023-06-04 12:12:26 +0300

    navigate the favouriteIcon to favouritePage

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -114,7 +114,7 @@ const TopNav = () => {
           </Stack>
           <Stack direction={"row"} alignItems={"center"} spacing={3}>
             <Stack spacing={2.5} direction="row">
-              <Link>
+              <Link href="/favourite">
                 <Badge
                   badgeContent={Favourite.badgeNumber}
                   showZero
