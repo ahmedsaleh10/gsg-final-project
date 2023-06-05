54794e7b6221a10d421c840563c6d574e3cad7df
Ahmed Saleh <ahmed.ovicsaleh@outlook.com>
2023-06-05 12:53:41 +0300

    adding cart route

--- src/Routers.js
+++ src/Routers.js
@@ -5,6 +5,7 @@ import ProductDetailsPage from './Pages/ProductDeatilsPage'
 import MenPage from './Pages/MenPage'
 import WomenPage from './Pages/WomenPage'
 import FavouritePage from './Pages/FavouritePage'
+import CartPage from './Pages/CartPage'
 
 const Routing = () => {
   return (
@@ -15,6 +16,7 @@ const Routing = () => {
             <Route path='/men' element={<MenPage/>}/>
             <Route path='/women' element={<WomenPage/>}/>
             <Route path='/favourite' element={<FavouritePage/>}/>
+            <Route path='/cart' element={<CartPage/>} />
             <Route path='*' element={<>not found</>} />
         </Routes>
     </BrowserRouter>
