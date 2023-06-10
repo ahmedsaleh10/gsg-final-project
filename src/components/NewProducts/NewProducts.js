87ae5c349e046fd899f27bfe31f2dcd5d701f631
Ahmed Saleh <ahmed.ovicsaleh@outlook.com>
2023-06-10 23:59:43 +0300

    implement NewProducts section

---
+++ src/components/NewProducts/NewProducts.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/grid";
+import "swiper/css/navigation";
+import ProductCard from '../ProductCard/ProductCard';
+import useStyles from "./Styles";
+import { Grid,Navigation } from "swiper";
+import { Typography,Stack } from "@mui/material";
+
+const NewProducts = ({ products }) => {
+  const unSaleProduct = products.filter((product)=> product.sale === false)
+  const [productsArray, setProductArray] = useState(unSaleProduct);
+  const [activeButton,setActiveButton]=useState("All")
+  const classes = useStyles()
+
+  return (
+    <div id='Swiper' className={classes.sliderContainer}>
+        <Stack direction={{xs:'column',lg:'row'}} alignItems={'center'}>
+            <Typography variant="subtitle1" className={classes.containerTitle} textAlign={'center'}> New Products </Typography>
+            <Stack direction={{xs:'column',sm:"row"}} spacing={{xs:2}} className={classes.buttonStack}>
+                <button onClick={()=> {setProductArray(unSaleProduct); setActiveButton("All")}} className={activeButton === "All" && classes.activeButton}>All</button>
+                <button onClick={()=> {setProductArray(unSaleProduct.filter((product)=> product.category === `men's clothing`)); setActiveButton("Shop Men's")}} className={activeButton === "Shop Men's" && classes.activeButton}>Shop Men's</button>
+                <button onClick={()=> {setProductArray(unSaleProduct.filter((product)=> product.category === `women's clothing`)); setActiveButton("Shop Women's")}} className={activeButton === "Shop Women's" && classes.activeButton}> Shop Women's</button>
+            </Stack>
+        </Stack>
+        <Swiper
+            navigation={true}
+            loop={true}
+            grid={{
+            rows: 1,
+            }}
+            modules={[Grid,Navigation]}
+            centeredSlides={true}
+            className={classes.customSwiper}
+            breakpoints={{
+            300: {
+                slidesPerView: 1,
+                grid: {
+                rows: 3,
+                },
+                spaceBetween: 20,
+            },
+            600: {
+                slidesPerView: 2,
+                centeredSlides:false,
+                grid: {
+                rows: 2,
+                },
+                spaceBetween: 30,
+            },
+            1200: {
+                slidesPerView: 4,
+                grid: {
+                rows: 1,
+                },
+                spaceBetween: 30,
+            },
+            }}
+        >
+            {productsArray.map((product) => {
+            return (
+                <SwiperSlide key={product.id}>
+                {<ProductCard product={product} />}
+                </SwiperSlide>
+            );
+            })}
+        </Swiper>
+    </div>
+  );
+};
+
+export default NewProducts;
---
+++ src/components/NewProducts/Styles.js
@@ -0,0 +1,46 @@
+import { createUseStyles } from "react-jss";
+import theme from "../../theme";
+
+const useStyles = createUseStyles({
+    sliderContainer:{
+        margin:'1.5rem 0',
+        overflow:'hidden',
+        [theme.breakpoints.down('lg')]:{
+            margin:'2rem',
+        }
+    },
+    containerTitle:{
+        margin:'0rem 8rem',
+        [theme.breakpoints.down('lg')]:{
+            margin:'2rem',
+        } 
+    },
+    customSwiper:{
+        marginTop:"1.5rem",
+        overflow:"visible",
+        '& div':{
+            '& div':{
+                maxHeight:'577px',
+                overflow:'hidden'
+            }
+        }
+    },
+    buttonStack:{
+        '& button':{
+            height:'50px',
+            width:'180px',
+            fontSize:'1.1rem',
+            fontFamily:'Poppins',
+            border:'none',
+            cursor:'pointer',
+            fontWeight:500,
+        }
+    },
+    activeButton:{
+        backgroundColor:theme.palette.primary.dark,
+        color:theme.palette.common.white,
+    }
+
+})
+
+export default useStyles
